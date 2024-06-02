e7109ef6a3912594f937c7c11c740582e7a090cf
David Jozwik <david@djoz.us>
2024-06-02 04:36:35 -0500

    Flipped DP processing finished, start with the last flower array item and work up to the first. Added colored messages. Added random delay to simulation messages

--- helpers.py
+++ helpers.py
@@ -1,13 +1,31 @@
+import enum
 import os
 
+
 # Format and print the dynamic programming table (2D Matrix)
-def printMatrix(matrix, title, reverse=False):
-    matrix = matrix[::-1] if reverse else matrix
-
-    print(title)
-    print('\n'.join([''.join(['{:4}'.format(item) for item in row]) for row in matrix]))
+def print_matrix(matrix, title):
+    print_info_message(title)
+    print_info_message('\n'.join([''.join(['{:4}'.format(item) for item in row]) for row in matrix]))
 
 
-def printInfoMessage(message):
+def print_info_message(message):
     if os.environ.get("DEBUG"):
-        print(f"INFO: {message}")
+        print_in_color(f"INFO: {message}", MessageColor.CYAN)
+
+
+# Enum to represent the color of the message and corresponding ANSI escape codes/VT Sequences
+class MessageColor(enum.Enum):
+    RED = '\033[91m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    BLUE = '\033[94m'
+    MAGENTA = '\033[95m'
+    CYAN = '\033[96m'
+    WHITE = '\033[97m'
+    RESET = '\033[0m'
+
+
+def print_in_color(message, color):
+    if not isinstance(color, MessageColor):
+        raise ValueError("color must be an instance of the Color enum")
+    print(f"{color.value}{message}{MessageColor.RESET.value}")
--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-from flowers import Flower, flowers as avail_flowers
-from helpers import printMatrix, printInfoMessage
+import math
+import random
+import time
+
+from flowers import flowers as avail_flowers
+from helpers import print_matrix, print_info_message, print_in_color, MessageColor
 
 
 def bee_simulator(flowers, flower_index, bee_energy, dp, total_flowers, choices):
     # BASE CASE: The bee has no more energy (columns) or there are no more flowers to visit (rows).
     # If you were writing the DP table by hand, this would be the first row and column.
-    if bee_energy <= 0 or flower_index < total_flowers:
+    if bee_energy <= 0 or flower_index < 0:
         return 0
 
     # If the current cell in the DP table has already been calculated (memoized), return the value.
@@ -15,21 +19,19 @@ def bee_simulator(flowers, flower_index, bee_energy, dp, total_flowers, choices)
     # Select the current flower we will be evaluating
     current_flower = flowers[flower_index]
 
-    print(f"Considering {current_flower.type} (Row: {flower_index}, Column: {bee_energy})");
-
     # If the current flower's energy cost is greater than the bee's remaining energy,
     # we skip this flower and move on to the next.
     if current_flower.energy_cost > bee_energy:
-        printInfoMessage(f"Skipping {current_flower.type} due to high energy cost. Needed Energy: {current_flower.energy_cost}. Available energy: {bee_energy}. Deficit: {bee_energy - current_flower.energy_cost}  (Row: {flower_index}, Column: {bee_energy})")
-        return bee_simulator(flowers, flower_index + 1, bee_energy, dp, total_flowers, choices)
+        print_info_message(f"Skipping {current_flower.type} due to high energy cost. Needed Energy: {current_flower.energy_cost}. Available energy: {bee_energy}. Deficit: {bee_energy - current_flower.energy_cost}  (Row: {flower_index}, Column: {bee_energy})")
+        return bee_simulator(flowers, flower_index - 1, bee_energy, dp, total_flowers, choices)
     else:
         # Perform a comparison between two scenarios:
         # 1. The bee collects the nectar and pollen from the current flower
         # 2. The bee skips the current flower and moves on to the next one
         # The optimal solution is the maximum nectar value of these two scenarios.
-        collect = current_flower.nectar + bee_simulator(flowers, flower_index + 1,
+        collect = current_flower.nectar + bee_simulator(flowers, flower_index - 1,
                                                         bee_energy - current_flower.energy_cost, dp, total_flowers, choices)
-        not_collect = bee_simulator(flowers, flower_index + 1, bee_energy, dp, total_flowers, choices)
+        not_collect = bee_simulator(flowers, flower_index - 1, bee_energy, dp, total_flowers, choices)
 
         # Determine which scenario yields the maximum nectar value
         # We'll also keep track of the decision made for each flower in the choices table,
@@ -47,7 +49,7 @@ def bee_simulator(flowers, flower_index, bee_energy, dp, total_flowers, choices)
         dp[flower_index][bee_energy] = result
 
         # Print information about the current flower and the decision-making process
-        printInfoMessage(f"""
+        print_info_message(f"""
         Considering {current_flower.type} (Row: {flower_index}, Column: {bee_energy})
             {current_flower.nectar} (+{current_flower.nectar} nectar), {current_flower.energy_cost} (-{current_flower.energy_cost} energy), 
             New total nectar if collected: {collect}
@@ -64,19 +66,34 @@ def print_collected_flowers(flowers, bee_energy, choices):
     # Start the bee with no collected flowers and maximum energy
     collected_flowers = []
     e = bee_energy
+    total_nectar = 0
 
-    for i in range(len(flowers)):
+    # Start in the bottom right corner of the choices table,
+    for i in reversed(range(len(flowers))):
+        # If the bee chose to collect the current flower, it is part of the optimal solution
         if choices[i][e]:
-            collected_flowers.append((flowers[i].type, flowers[i].nectar, flowers[i].energy_cost))
+            total_nectar += flowers[i].nectar
+            flower_info = (flowers[i].type, flowers[i].nectar, flowers[i].energy_cost, e, total_nectar)
+            collected_flowers.append(flower_info)
+
+            # Move the bee's energy to the left by the energy cost of the flower
+            # We'll keep evaluating from here until the bee is out of energy or there are no more flowers
             e -= flowers[i].energy_cost
+
+
+    # Main simulation loop
     for flower in collected_flowers:
-        print(f"Bee collected {flower[1]} nectar from a {flower[0]}, expending {flower[2]} energy")
+        # Simulate the bee collecting nectar from the flower by sleeping for a random amount of time
+        # The delay is proportional to the energy cost of the flower and is a random value between half and the full energy cost in seconds
+        delay = random.uniform(math.ceil(flower[2] / 2), flower[2])
+        time.sleep(delay)
+        print_in_color(f"Bee collected {flower[1]} nectar from a {flower[0]}, expending {flower[2]} energy. (Total Nectar: {flower[4]}) (Remaining Energy: {flower[3]})", MessageColor.GREEN)
 
 
 # Initialize parameters for the bee simulator and call the function
 def main():
     # Bee energy is akin to the weight limit of the knapsack problem
-    bee_energy = 10
+    bee_energy = 100
 
     total_flowers = len(avail_flowers)
     # Initialize the dynamic programming table used for memoization
@@ -86,15 +103,15 @@ def main():
     # Initialize the choices table to keep track of the flowers the bee chooses
     choices = [[False] * (bee_energy + 1) for _ in range(total_flowers)]
 
-    reversed_flowers = avail_flowers[::-1]
-
     # Call the bee simulator function to find the optimal solution
-    result = bee_simulator(reversed_flowers, 0, bee_energy, dp, total_flowers, choices)
+    result = bee_simulator(avail_flowers, total_flowers - 1, bee_energy, dp, total_flowers, choices)
 
-    printMatrix(dp, "Dynamic Programming Table (2D Matrix)", True)
-    printMatrix(choices, "Choices Table (2D Matrix)")
-    print(f"Maximum nectar collected by the bee: {result}")
+    print_matrix(dp, "Dynamic Programming Table (2D Matrix)")
+    print_matrix(choices, "Choices Table (2D Matrix)")
+    print_in_color(f"☀️ Your bee has woken up in a beautiful meadow on a sunny day and is ready to start collecting nectar! 🌻", MessageColor.YELLOW)
     print_collected_flowers(avail_flowers, bee_energy, choices)
+    print_in_color("🌙 The day is over and your bee is sleepy, they have returned to the hive for a good night's rest... 💤", MessageColor.BLUE)
+    print_in_color(f"Total nectar collected by your bee: {result}. Great Job! ⭐", MessageColor.MAGENTA)
 
 
 if __name__ == "__main__":
